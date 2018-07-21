768ab97d2da5669976a7768ac39e0d63be59a611
haykam821 <24855774+haykam821@users.noreply.github.com>
2018-07-20 22:18:18 -0400

    Remove SQLite stuff

--- index.js
+++ index.js
@@ -5,14 +5,8 @@ const cmdo = require("discord.js-commando");
 const client = new cmdo.Client({
 	owner: config.discord.owner,
 });
-const sqlite = require("sqlite");
 
 const path = require("path");
-
-client.setProvider(
-	sqlite.open(path.join(__dirname, "settings.sqlite3")).then(db => new cmdo.SQLiteProvider(db))
-);
-
 client.registry.registerGroups([
 	["pxls", "Pxls.space"],
 	["info", "Info"],
